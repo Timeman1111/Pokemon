437657b9a7d9fda50708d34a45e75cfbd66f7efb
Yeeden7 <yeedenchungus@gmail.com>
2024-12-18 19:39:26 -0500

    Update POkemon.java

--- src/POkemon.java
+++ src/POkemon.java
@@ -1,11 +1,24 @@
 public class POkemon {
-    public birth(int hp, int atk, int def,, int spatk, int spdef, int speed, String type1, String type2)
+    private int health;
+    private int attack;
+    private int defense;
+    private int specialAttack;
+    private int specialDefense;
+    private int speed;
 
 
 
+    public birth(int hp, int atk, int def,, int spatk, int spdef, int spd, String type1, String type2){
+        health = hp;
+        attack = atk;
+        defense = def;
+        specialAttack =spatk;
+        specialDefense = spdef;
+        speedStat = spd;
 
 
-    private int hp =
+    }
+
 
 
 }
