77a8d25d6b2e3c0360df3fe0364fed8b792a28a6
Ethan Small <78430095+Timeman1111@users.noreply.github.com>
2024-12-18 20:20:09 -0500

    Update Game.java
    added a few useful methods

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,28 @@
 public class Game {
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public void delay(int milliseconds){
+        try {
+           Thread.sleep(milliseconds);
+       }catch(InterruptedException ex){
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void playIntro(){
+        clearScreen();
+        String line = "Hello there!";
+        line = Colors.RED + line + Colors.ANSI_RESET;
+
+        System.out.println(line);
+
+        delay(2000);
+
+        line = "Welcome to our Pokemon (unofficial) Battle Simulator!";
+        System.out.println(line);
+    }
 }
