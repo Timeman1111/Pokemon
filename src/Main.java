0ae2a9abcdfcfaf67dafb0e323bec20191fac6c5
Ethan Small <78430095+Timeman1111@users.noreply.github.com>
2024-12-18 20:03:39 -0500

    Removed testing stuff

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
-        String result = Colors.formatText("HELLO", Colors.BLUE);
-        Game.printDamage(5);
-        System.out.println(result);
     }
 }
