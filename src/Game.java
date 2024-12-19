b215bd8c3b7ec929de7d4171187449548d60cabc
Ethan Small <78430095+Timeman1111@users.noreply.github.com>
2024-12-18 20:23:43 -0500

    Update Game.java
    added comments

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,13 @@
 public class Game {
 
+
+    // Clears terminal screen (DOES NOT WORK IN INTELLIJ)
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
+    //  Delays code execution
     public void delay(int milliseconds){
         try {
            Thread.sleep(milliseconds);
@@ -13,6 +16,8 @@ public class Game {
         }
     }
 
+
+    // Writes out the intro
     public void playIntro(){
         clearScreen();
         String line = "Hello there!";
@@ -23,6 +28,11 @@ public class Game {
         delay(2000);
 
         line = "Welcome to our Pokemon (unofficial) Battle Simulator!";
+        System.out.println(line);
+
+        delay(1000);
+
+        line = "\nMade by Ethan Small & Eden Chung";
         System.out.println(line);
     }
 }
