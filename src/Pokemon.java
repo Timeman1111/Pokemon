f4895cd535a5322b7d4818e29167b9a42d1d11e8
Yeeden7 <yeedenchungus@gmail.com>
2024-12-18 19:49:45 -0500

    added

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,22 +1,27 @@
-public class Pokémon {
+public class Pokemon {
+    //all the pokemons stats and typing
     private int health;
     private int attack;
     private int defense;
     private int specialAttack;
     private int specialDefense;
     private int speed;
+    private String typing1;
+    private String typing2;
+    private int
 
 
 
     public birth(int hp, int atk, int def,, int spatk, int spdef, int spd, String type1, String type2){
+        //sets all the stats
         health = hp;
         attack = atk;
         defense = def;
         specialAttack =spatk;
         specialDefense = spdef;
         speed = spd;
-
-
+        typing1 = type1;
+        typing2 = type2;
     }
 
 
