f6c788c9f2d542a634bca6b2441dca8cd203dfe5
Yeeden7 <yeedenchungus@gmail.com>
2024-12-18 20:17:29 -0500

    Update Pokemon.java

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,6 +1,7 @@
 public class Pokemon {
     //all the pokemons stats and typing
-    private int health;
+    private int level;
+    private int maxHealth;
     private int currentHealth;
     private int attack;
     private int defense;
@@ -20,9 +21,10 @@ public class Pokemon {
 
 
 //makes new pokemon
-    public void birth(int hp, int atk, int def, int spatk, int spdef, int spd, String type1, String type2){
+    public void birth(int lvl, int hp, int atk, int def, int spatk, int spdef, int spd, String type1, String type2){
         //sets all the stats
-        health = hp;
+        maxHealth = hp;
+        currentHealth = hp;
         attack = atk;
         defense = def;
         specialAttack =spatk;
@@ -32,7 +34,35 @@ public class Pokemon {
         typing2 = type2;
     }
 
-    //public int get
+    public int getAttack(){
+        // gets the attack stat of a pokeomn
+        if(atkBoost > 6)
+            atkBoost = 6;
+        return attack * atkBoost;
+    }
+
+    public int getDefense(){
+        if (defBoost > 6)
+            defBoost = 6;
+        return defense * defBoost;
+    }
+
+    public int getSpecialAttack(){
+        if (spatkBoost > 6)
+            spatkBoost = 6;
+        return specialAttack * spatkBoost;
+    }
+
+    public int getSpecialDefense(){
+        if (spdefBoost > 6)
+            spdefBoost = 6;
+        return specialDefense * spdefBoost;
+    }
+    public int getSpeed(){
+        if (speedBoost > 6)
+            speedBoost = 6;
+        return speed * speedBoost;
+    }
 
 
 
