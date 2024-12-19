59b2002606867e5c306513d00bb286801e82262c
Yeeden7 <yeedenchungus@gmail.com>
2024-12-18 19:34:36 -0500

    Update POkemon.java

--- src/POkemon.java
+++ src/POkemon.java
@@ -1,5 +1,11 @@
 public class POkemon {
-    private int hp = 
+    public birth(int hp, int atk, int def,, int spatk, int spdef, int speed, String type1, String type2)
+
+
+
+
+
+    private int hp =
 
 
 }
